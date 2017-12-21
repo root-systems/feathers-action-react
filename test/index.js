e78fc88e0413a76905b519e7c6e0c5929991f787
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-12-21 17:24:50 +1100

    basic rendering of component with mock redux store

--- test/index.js
+++ test/index.js
@@ -1,18 +1,35 @@
 import test from 'ava'
 import { createElement as h } from 'react'
+import configureStore from 'redux-mock-store'
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
+
 import { connect } from '../'
 
 test.before(t => {
   Enzyme.configure({ adapter: new Adapter() })
 })
 
+test.beforeEach(t => {
+  const middlewares = []
+  const mockStore = configureStore(middlewares)
+  const initialState = {}
+  const store = mockStore(initialState)
+
+  t.context.store = store
+})
+
 test('component is legit', function (t) {
   function TestComponent() {
     return h('div', {})
   }
-  const wrapper = mount((h(TestComponent)))
+  const connectedComponent = connect({})(TestComponent)
+
+  const wrapper = mount(
+    h(connectedComponent, {
+      store: t.context.store
+    })
+  )
 
   t.true(wrapper.exists())
 })
