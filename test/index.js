2572d094eee9d38f5d8986d5000d6dcf7f54a5f1
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-12-21 17:04:00 +1100

    basic DOM rendering test working

--- test/index.js
+++ test/index.js
@@ -1,7 +1,18 @@
 import test from 'ava'
-
+import { createElement as h } from 'react'
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
 import { connect } from '../'
 
-test('feathers-action-react', function (t) {
-  t.is(typeof connect, 'function', 'connect is a function')
+test.before(t => {
+  Enzyme.configure({ adapter: new Adapter() })
+})
+
+test('component is legit', function (t) {
+  function TestComponent() {
+    return h('div', {})
+  }
+  const wrapper = mount((h(TestComponent)))
+
+  t.true(wrapper.exists())
 })
