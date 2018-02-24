7fc420081725dd3ee352500c1466dc6397d43f66
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2018-02-24 23:18:30 +1300

    add check for if query property is required

--- test/index.js
+++ test/index.js
@@ -5,3 +5,20 @@ import { connect } from '../'
 test('feathers-action-react', function (t) {
   t.is(typeof connect, 'function', 'connect is a function')
 })
+
+test('options.query can be a single query object', function (t) {
+  const result = connect({
+    selector: () => null,
+    actions: {
+      dogs: {
+        test: () => null
+      }
+    },
+    query: {
+      service: 'dogs'
+    }
+  })
+
+  // TODO: IK: not really a sufficient test
+  t.is(typeof result, 'function', 'result of connect is a function')
+})
